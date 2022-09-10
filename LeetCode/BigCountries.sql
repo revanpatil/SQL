2fb21e38f6c1c0949f0944666759e2c260e33a25
Revan Patil <108737061+revanpatil@users.noreply.github.com>
2022-09-10 18:13:26 +0530

    Update BigCountries.sql

--- LeetCode/BigCountries.sql
+++ LeetCode/BigCountries.sql
@@ -1,4 +1,4 @@
-# https://leetcode.com/problems/big-countries/
+-- https://leetcode.com/problems/big-countries/
 # Write your MySQL query statement below
 Select name,population,area from World where population >= 25000000 OR area >= 3000000;
 
